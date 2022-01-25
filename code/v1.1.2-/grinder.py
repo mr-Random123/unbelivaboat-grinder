a0f8a69877eb436e1a67d9633d19bd40e400576a
SupDarkTRoop <jmiguel092573@gmail.com>
2022-01-25 18:51:09 +0300

    new file
    w

--- code/v1.1.2-/grinder.py
+++ code/v1.1.2-/grinder.py
@@ -1,112 +1,150 @@
 import time
 import random
 import keyboard
-print("HELLO NEW USER or not...")
-time.sleep(1)
-exit_I = str.lower(input("do you want to run this?(Y/N): "))
+#--one function use--
+from tkinter import messagebox
+import webbrowser
+#from os.path import dirname, abspath
 
-if exit_I[str.find("y", exit_I)] == "y":
-    print("OKAY THEN")
+version = [1, 1, 3]
+nversion = []
+def update():
+    updatebox = messagebox.askquestion(title="update", message="it seems you got the old version press yes to update")
+    if updatebox == "yes":
+        webbrowser.open_new("https://github.com/mr-Random123/unbelivaboat-grinder/tree/main/exes")
+        exit()
+def main():
+    print("HELLO NEW USER or not...")
     time.sleep(1)
-    print("RUNNING")
+    exit_I = str.lower(input("do you want to run this?(Y/N): "))
 
-elif exit_I[str.find("n", exit_I)] == "n":
-    print("BYE!")
-    time.sleep(1)
-    exit()
-
-else:
-    print("BYE!")
-    time.sleep(1)
-    exit()
-
-print("alright we just need a bit of input so please answer")
-time.sleep(1)
-
-word_0_input = str.lower(input("do !dep all?(Y/N): "))
-word_1_input = str.lower(input("do !bal?(Y/N): "))
-word_2_input = str.lower(input("do !collect?(Y/N): "))
-
-number_0_input = 0
-if word_2_input[str.find("y", exit_I)] == "y":
-    try:
-        number_0_input = int(input("how many minutes per !collect?(only in minutes): "))
-    except int:
-        print("bro fr??")
+    if exit_I[str.find("y", exit_I)] == "y":
+        print("OKAY THEN")
         time.sleep(1)
-        print("pls put in a valid number next time")
-        time.sleep(4)
+        print("RUNNING")
+
+    elif exit_I[str.find("n", exit_I)] == "n":
         print("BYE!")
         time.sleep(1)
         exit()
 
-number_1_input = float(input("a delay :troll: how much seconds?(put 0 for no delay): "))
-
-
-minutes_passed = 0
-reset_minutes = 0
-
-word = "!work"
-word_0 = "!dep all"
-word_1 = "!bal"
-word_2 = "!collect"
-
-print("!dep all = " + word_0_input)
-print("!bal = " + word_1_input)
-print("!collect = " + word_2_input)
-print("!collect per minutes = " + str(number_0_input))
-print("delay = " + str(number_1_input))
-print("exit this console to stop the process")
-time.sleep(10)
-print("\n"*2)
-
-while True:
-
-    rng = random.randint(1,3)
-    rng_0 = random.randint(1, 5)
-    wait = 60
-
-    print("minutes passed: " + str(minutes_passed))
-    print(wait)
-
-    for index in range(1, wait, 1):
-        print(wait-index)
+    else:
+        print("BYE!")
         time.sleep(1)
-    time.sleep(number_1_input)
-    minutes_passed += 1
-    reset_minutes += 1
+        exit()
 
-    print(minutes_passed)
+    print("alright we just need a bit of input so please answer")
+    time.sleep(1)
+
+    word_0_input = str.lower(input("do !dep all?(Y/N): "))
+    word_1_input = str.lower(input("do !bal?(Y/N): "))
+    word_2_input = str.lower(input("do !collect?(Y/N): "))
+
+    number_0_input = 0
+    if word_2_input[str.find("y", exit_I)] == "y":
+        try:
+            number_0_input = int(input("how many minutes per !collect?(only in minutes): "))
+        except int:
+            print("bro fr??")
+            time.sleep(1)
+            print("pls put in a valid number next time")
+            time.sleep(4)
+            print("BYE!")
+            time.sleep(1)
+            exit()
+
+    number_1_input = float(input("a delay :troll: how much seconds?(put 0 for no delay): "))
+
+
+    minutes_passed = 0
+    reset_minutes = 0
+
+    word = "!work"
+    word_0 = "!dep all"
+    word_1 = "!bal"
+    word_2 = "!collect"
+
+    print("!dep all = " + word_0_input)
+    print("!bal = " + word_1_input)
+    print("!collect = " + word_2_input)
+    print("!collect per minutes = " + str(number_0_input))
+    print("delay = " + str(number_1_input))
+    print("exit this console to stop the process")
+    time.sleep(10)
     print("\n"*2)
-    print("!work")
 
-    keyboard.write(word)
-    keyboard.press('enter')
+    while True:
 
-    if rng == 2 and word_0_input[str.find("y", exit_I)] == "y":
+        rng = random.randint(1,3)
+        rng_0 = random.randint(1, 5)
+        wait = 60
 
-        time.sleep(1.5)
+        print("minutes passed: " + str(minutes_passed))
+        print(wait)
 
-        print("!dep all")
+        for index in range(1, wait, 1):
+            print(wait-index)
+            time.sleep(1)
+        time.sleep(number_1_input)
+        minutes_passed += 1
+        reset_minutes += 1
 
-        keyboard.write(word_0)
+        print(minutes_passed)
+        print("\n"*2)
+        print("!work")
+
+        keyboard.write(word)
         keyboard.press('enter')
 
-    if rng_0 == 3 and word_1_input[str.find("y", exit_I)] == "y":
+        if rng == 2 and word_0_input[str.find("y", exit_I)] == "y":
 
-        time.sleep(1.5)
+            time.sleep(1.5)
 
-        print("!bal")
+            print("!dep all")
 
-        keyboard.write(word_1)
-        keyboard.press('enter')
-    if reset_minutes >= number_0_input and word_2_input[str.find("y", exit_I)] == "y":
-        reset_minutes = 0
-        time.sleep(1.5)
+            keyboard.write(word_0)
+            keyboard.press('enter')
 
-        print("!collect")
+        if rng_0 == 3 and word_1_input[str.find("y", exit_I)] == "y":
 
-        keyboard.write(word_2)
-        keyboard.press('enter')
-    print("\n" * 2)
+            time.sleep(1.5)
 
+            print("!bal")
+
+            keyboard.write(word_1)
+            keyboard.press('enter')
+        if reset_minutes >= number_0_input and word_2_input[str.find("y", exit_I)] == "y":
+            reset_minutes = 0
+            time.sleep(1.5)
+
+            print("!collect")
+
+            keyboard.write(word_2)
+            keyboard.press('enter')
+        print("\n" * 2)
+
+
+if __name__ == '__main__':
+    reuse_table0 = []
+    reuse_table1 = []
+    f = open("version.txt", "r+")
+    for letter in f.readline():
+        reuse_table1.append(int(letter))
+
+    for letter in reuse_table1:
+        reuse_table0.append(letter)
+    reuse_table1.pop()
+    for letter in reuse_table0:
+        nversion.append(letter)
+
+    if version[0] == nversion[1]:
+
+        if version[1] == nversion[1]:
+
+            if version[2] <= nversion[2]:
+                update()
+        else:
+            update()
+    else:
+        update()
+    main()
